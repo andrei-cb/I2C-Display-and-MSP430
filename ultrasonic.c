4b74f9608a8b956a8b3efdab9ee822205798233d
Andrei <caydenz@protonmail.ch>
2017-01-21 19:18:20 +0200

    updated comments

--- ultrasonic.c
+++ ultrasonic.c
@@ -3,8 +3,8 @@
 
 void UltrasonicInit()
 {
-    P1DIR |= TRIG_PIN; //set trigger pin as output
-    TA1CTL = TASSEL_2 + ID_0 + MC_2; //increment timer TA1R every microsecond
+    P1DIR |= TRIG_PIN; // Set trigger pin as output
+    TA1CTL = TASSEL_2 + ID_0 + MC_2; // Increment timer TA1R every microsecond (SMCLK + no divider + continuous mode)
 }
 
 float UltrasonicGetDistance()
@@ -12,17 +12,17 @@ float UltrasonicGetDistance()
     int echoDuration;
     float distance;
 
-    P1OUT |= TRIG_PIN; // trigger high
+    P1OUT |= TRIG_PIN; // Trigger high
 
     __delay_cycles(20);
     
-    P1OUT &= ~TRIG_PIN; // trigger  low
+    P1OUT &= ~TRIG_PIN; // Trigger low
     
-    while ((P1IN & ECHO_PIN) == 0); //wait until echo pulse starts
+    while ((P1IN & ECHO_PIN) == 0); // Wait until echo pulse starts
     TA1R = 0;
-    while ((P1IN & ECHO_PIN) > 0); //wait for the end of echo pulse
+    while ((P1IN & ECHO_PIN) > 0); // Wait for the end of echo pulse
     
-    echoDuration = TA1R; //timer value is the echo duration
-    distance = echoDuration / 58.0; //distance is echo duration divided by 58
+    echoDuration = TA1R; // Timer value is the echo duration
+    distance = echoDuration / 58.0; // Distance is echo duration divided by 58
     return distance;
 }
